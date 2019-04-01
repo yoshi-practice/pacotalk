53ebbff0bead9f99f9587924c88b15e30477c4d1
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-04-01 10:22:45 +0900

    [Fix] やっぱりangularだなぁと猛省

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { NgModule } from '@angular/core';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage,
-    ListPage
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    AppRoutingModule
   ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
