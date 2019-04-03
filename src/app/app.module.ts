656c24d9d00a77e184790007304479f683547b6c
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-04-03 11:03:47 +0900

    [Update] Firebase雛形を追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { environment } from './../environments/environment';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guard/auth.guard';
+import { UserLoginComponent } from './pages/user-login/user-login.component';
+import { UserSignupComponent } from './pages/user-signup/user-signup.component';
+import { UserInfoComponent } from './pages/user-info/user-info.component';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -16,6 +28,10 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     BrowserAnimationsModule,
     FlexLayoutModule,
     MaterialModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase), 
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,6 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -14,3 +10,15 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDIWGStoVFvsKNdPHCRgMHbATkb1J3FtVk',
+    authDomain: 'paco-talk.firebaseapp.com',
+    databaseURL: 'https://paco-talk.firebaseio.com',
+    projectId: 'paco-talk',
+    storageBucket: 'paco-talk.appspot.com',
+    messagingSenderId: '525516352945'
+  }
+};
