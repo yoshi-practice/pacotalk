ec4de59061834b2b772d8363cdbe775799739502
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-04-03 12:34:11 +0900

    [Update] signin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { ReactiveFormsModule } from '@angular/forms';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { environment } from './../environments/environment';
-import { AuthService } from './services/auth.service';
-import { AuthGuard } from './guard/auth.guard';
-import { UserLoginComponent } from './pages/user-login/user-login.component';
-import { UserSignupComponent } from './pages/user-signup/user-signup.component';
-import { UserInfoComponent } from './pages/user-info/user-info.component';
+import { SigninComponent } from './signin/signin.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
@@ -29,9 +23,6 @@ import { UserInfoComponent } from './pages/user-info/user-info.component';
     FlexLayoutModule,
     MaterialModule,
     ReactiveFormsModule,
-    AngularFireModule.initializeApp(environment.firebase), 
-    AngularFireAuthModule,
-    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
